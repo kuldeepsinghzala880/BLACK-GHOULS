1be107ff9e248015011dd369fabc248adf311d44
kuldeepsinghzala880 <77768635+kuldeepsinghzala880@users.noreply.github.com>
2021-01-21 17:15:54 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -41,7 +41,6 @@ class Var(object):
         except ValueError:
             raise ValueError("Invalid Private Group ID. Make sure your ID is starts with -100 and make sure that it is only numbers.")
 
-
 class Development(Var):
     LOGGER = True
     # Here for later purposes
