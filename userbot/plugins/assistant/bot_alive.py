2596994081a5eff6da247e1eab9f436431cf63ed
kuldeepsinghzala880 <77768635+kuldeepsinghzala880@users.noreply.github.com>
2021-01-22 17:12:21 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -3,19 +3,19 @@ from telethon import events
 from userbot import ALIVE_NAME, bot
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/22e70478bd48568451a1f.jpg"
+PM_IMG = "https://telegra.ph/file/a255957ef156701eb5d42.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
-pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
-pm_caption += "➥ **Python:** `3.8.6` \n"
+pm_caption += "➥ **Telethon Version:** `1.19.0` \n"
+pm_caption += "➥ **Python:** `3.9.1` \n"
 pm_caption += "➥ **Database Status:**  `Functional`\n"
 pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `2.0`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/DARK-COBRA/DARKCOBRA/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [DarkCobra](https://t.me/Dark_cobra_support_group)\n"
-pm_caption += "[Assistant By DarkCobra](https://t.me/Dark_cobra_support_group)"
+pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/kuldeepsinghzala880/BLACK-GHOULS/blob/main/LICENSE)\n"
+pm_caption += "➥ **Copyright** : By [BLACK-GHOULS](https://t.me/blackghouls_supportgroup)\n"
+pm_caption += "[Assistant By BLACKGHOULS](https://t.me/blackghouls_channel)"
 
 # only Owner Can Use it
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
