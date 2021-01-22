44ab518afcee97aa09c9781772cd817b50a0a2e6
kuldeepsinghzala880 <77768635+kuldeepsinghzala880@users.noreply.github.com>
2021-01-22 16:27:24 +0530

    BG

---
+++ userbot/helpers/styles/fun.py
@@ -0,0 +1,32 @@
+from userbot.functions.funts import BG_STYLE as BG_FONT_STYLE
+from userbot.functions.funts import BLACKFONTS as BGFONTS
+from userbot.functions.funts import SIMPLESTBG as SIMPLEBG
+
+
+
+
+
+#maa chod dunga 
+
+
+
+
+
+
+#kya dekh raha hai madrchod
+
+
+
+
+
+
+
+#bhagg 
+
+
+
+
+
+
+
+#kang karega gand tod dunga
