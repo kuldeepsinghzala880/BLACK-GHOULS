58248d1761ad5ede1db5aba2255d5aeea31b19a1
BLACKSNAC <77516086+BLACKSNAC@users.noreply.github.com>
2021-01-23 18:30:00 +0530

    Update functions.py

--- userbot/helper/functions.py
+++ userbot/helper/functions.py
@@ -2,8 +2,7 @@ import requests , os, re, time
 from bs4 import BeautifulSoup
 from asyncio import sleep
 from random import choice
-from telethon import events
-from emoji import get_emoji_regexp
+from telethon import event
 from PIL import Image
 from validators.url import url
 from telethon.tl.types import Channel
