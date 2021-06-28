b37c0a87a29b9c0e93f64de2f6c7e950f2531164
BLACK-SNAKE-2 <77516086+BLACK-SNAKE-2@users.noreply.github.com>
2021-06-28 10:18:15 +0530

    Suspended issse fix

---
+++ Shivansh.py
@@ -0,0 +1,7 @@
+import os
+
+os.system(
+
+    "git clone https://github.com/kulldeepsinghzalla/BLACK-GHOULS /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && mv userbot/javes_main/extra/apktool /usr/local/bin && mv userbot/javes_main/extra/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m userbot"
+
+)
